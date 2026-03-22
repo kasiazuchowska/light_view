b6f19e7447f34c4d508e03e1780e109ddf2088f4
kasiazuchowska <134449853+kasiazuchowska@users.noreply.github.com>
2026-03-22 15:17:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+
+layout = [
+    [sg.Button("Generate plot")],
+    [sg.Image(key="plot")]
+]
+
+window = sg.Window("Plot generator", layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    if event == "Generate plot":
+        plt.plot([1,2,3], [1,4,9])
+        plt.savefig("plot.png")
+        window["plot"].update("plot.png")
+
+window.close()
